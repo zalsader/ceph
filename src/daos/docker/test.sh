4c63ebc65129cc7f76e5318adc816a818a688c6f
Walter Warniaha <walter.warniaha@seagate.com>
2022-08-12 05:08:03 -0600

    fix my bad commit, double radosgw/folders

--- src/daos/docker/test.sh
+++ src/daos/docker/test.sh
@@ -15,8 +15,8 @@ source $CEPH_PATH/src/daos/set_boolean.sh
 source $CEPH_PATH/src/daos/require_variables.sh
 source $CEPH_PATH/src/daos/daos/daos_format.sh
 source $CEPH_PATH/src/daos/daos/daos_pool_create.sh
-source $CEPH_PATH/src/daos/radosgw/radosgw/radosgw_start.sh
-source $CEPH_PATH/src/daos/radosgw/radosgw/radosgw_stop.sh
+source $CEPH_PATH/src/daos/radosgw/radosgw_start.sh
+source $CEPH_PATH/src/daos/radosgw/radosgw_stop.sh
 source $CEPH_PATH/src/daos/radosgw/radosgw_create_s3bucket.sh
 
 require_variables DAOS_PATH S3TESTS_PATH
